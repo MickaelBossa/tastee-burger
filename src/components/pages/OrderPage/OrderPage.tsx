2f69e2853d0ab9ff71b2f82da087493ab737c3fa
BOSSA Mickael <mickael.bossa@outlook.com>
2023-01-18 10:07:23 +0100

    ✨ F04 : Add all elements to OrderPage

--- src/components/pages/OrderPage/OrderPage.tsx
+++ src/components/pages/OrderPage/OrderPage.tsx
@@ -1,15 +1,21 @@
 import { useParams } from 'react-router-dom';
 import BackButton from '../../../ui/BackButton/BackButton';
 import Logo from '../../../ui/Logo/Logo';
+import { CgProfile } from 'react-icons/cg';
 
 export default function OrderPage() {
     const { username } = useParams();
 
     return (
         <div>
-            <Logo />
-            <h1>Bonjour {username}</h1>
-            <BackButton buttonLink="/" buttonText="Déconnexion" />
+            <nav>
+                <Logo />
+                <h1>Hey, {username}</h1>
+                <BackButton buttonLink="/" buttonText="Se déconnecter" />
+                <CgProfile
+                    style={{ width: '48px', height: '48px', color: 'gray' }}
+                />
+            </nav>
         </div>
     );
 }
