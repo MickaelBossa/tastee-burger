9975f96c44864326488d1d4e296996e08d33b097
Mickael Bossa <mickael.bossa@outlook.com>
2023-01-23 11:34:21 +0100

    ♻️ F05 : Create ProductCardText component

--- src/components/pages/OrderPage/ProductCards/ProductCard/ProductCard.tsx
+++ src/components/pages/OrderPage/ProductCards/ProductCard/ProductCard.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import { theme } from '../../../../../theme';
-import SubmitButton from '../../../../../ui/SubmitButton/SubmitButton';
-import { formatPrice } from '../../../../../utils/math';
+import ProductCardText from './ProductCardText/ProductCardText';
 
 interface ProductCardProps {
     id: number;
@@ -19,17 +18,7 @@ export default function ProductCard({ item }: { item: ProductCardProps }) {
             <div className="imgContainer">
                 <img src={item.imageSource} alt={item.imageSource} />
             </div>
-
-            <div className="textContainer">
-                <p className="productTitle">{item.title}</p>
-                <div className="priceAndButton">
-                    <p className="productPrice">{formatPrice(item.price)} €</p>
-                    <SubmitButton
-                        className="orderSubmitButton"
-                        label="Ajouter"
-                    />
-                </div>
-            </div>
+            <ProductCardText title={item.title} price={item.price} />
         </ProductCardStyled>
     );
 }
@@ -54,48 +43,5 @@ const ProductCardStyled = styled.div`
         img {
             max-height: 100%;
         }
-    }
-
-    .textContainer {
-        width: 200px;
-        height: 105px;
-        margin: 20px 20px 10px 20px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-    }
-
-    .productTitle {
-        font-family: 'Amatic SC';
-        font-size: ${theme.fonts.size.P4};
-        font-weight: ${theme.fonts.weights.bold};
-        width: 200px;
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        min-height: 2.6rem;
-    }
-
-    .priceAndButton {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-    }
-
-    .productPrice {
-        width: 95.2px;
-        height: 65px;
-        display: flex;
-        align-items: center;
-        margin-bottom: 14.81px;
-        color: ${theme.colors.primary};
-        font-weight: ${theme.fonts.weights.medium};
-        font-size: ${theme.fonts.size.P2};
-    }
-
-    .orderSubmitButton {
-        height: ${theme.fonts.size.P4};
-        width: 100px;
-        margin-bottom: 25px;
     }
 `;
